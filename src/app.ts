b909f1106fd177c001251316a001fd14067b5454
nishuldhakar <nishuldhakar.se@gmail.com>
2026-02-01 12:47:55 +0530

    initial

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/user.routes.js";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.get("/", (_req, res) => {
+  res.send("API running 🚀");
+});
